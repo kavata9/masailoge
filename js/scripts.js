f73425ee527dac570a8dc358ef4945d15d592919
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-03-19 16:22:05 +0300

    add to the folder structure

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+//carousel for the main page
+
+var imageSlider = document.getElementById('imagesSlider');
+
+var images =["url('../Masailoge/pics/pic1.jpg')" , "url('../Masailoge/pics/pic2.jpg')" , "url('../Masailoge/pics/pic9.jpg')"];
+var counter = 0;
+
+function carousel(){
+imageSlider.style.backgroundImage = images[counter];
+counter++;
+if(counter > 2 ){
+ counter = 0;
+}
+}
+setInterval(carousel, 2000);
+//This will make text appear as a fade in.
+var $el = $(".slideText1:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*100).fadeIn(200);
+});
+var $el = $(".slideText2:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*400).fadeIn(500);
+});
+var $el = $(".slideText3:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*600).fadeIn(700);
+});
+var $el = $(".slideText4:first"), text = $el.text(),
+  words = text.split(" ");
+
+var html = "";
+for (var i = 0; i < words.length; i++) {
+  html += "<span>" + words[i] + " </span>";
+};
+$el.html(html).children().hide().each(function(i){
+$(this).delay(i*900).fadeIn(800);
+});
